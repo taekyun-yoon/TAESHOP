f7936665e2d0150f05b68525037c5363cb709c2e
taekyun-yoon <tk.yoon0105@gmail.com>
2024-02-24 13:36:39 +0900

    feat: create Member(Dto, Entity)

---
+++ src/main/java/com/TAE/shopping/member/domain/entity/Member.java
@@ -0,0 +1,40 @@
+package com.TAE.shopping.member.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "MEMBER")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "MEMBER_ID")
+    private Long id;
+
+    private String email;
+    private String password;
+    private String nickname;
+
+    private UserRole role;
+
+    @Builder
+    public Member(String email, String password, String nickname) {
+        this.email = email;
+        this.password = password;
+        this.nickname = nickname;
+    }
+
+    public void updateProfile(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public void updatePassword(String password) {
+        this.password = password;
+    }
+}
---
+++ src/main/java/com/TAE/shopping/member/domain/entity/UserRole.java
@@ -0,0 +1,5 @@
+package com.TAE.shopping.member.domain.entity;
+
+public enum UserRole {
+    MEMBER, ADMIN, SELLER;
+}
